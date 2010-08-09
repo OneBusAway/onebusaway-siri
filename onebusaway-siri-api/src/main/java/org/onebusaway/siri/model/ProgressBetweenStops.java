d238ae38f3e62edbe13f015f1b06a6b750a78a47
David Turner <novalis@novalis.org>
2010-08-09 22:11:52 +0000

    note that we are not providing some ugly parts of siri

--- onebusaway-siri-api/src/main/java/org/onebusaway/siri/model/ProgressBetweenStops.java
+++ onebusaway-siri-api/src/main/java/org/onebusaway/siri/model/ProgressBetweenStops.java
@@ -1,5 +1,8 @@
 package org.onebusaway.siri.model;
 
+/* unused because keeping track of it would require 
+ * intrusive changes to onebusaway
+ */
 public class ProgressBetweenStops {
 
   public double LinkDistance;
