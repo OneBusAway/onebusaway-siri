ee13fecf9b704a012915a94e792fbf8daa98a4eb
David Turner <novalis@novalis.org>
2010-09-30 18:07:47 +0000

    distances (1/2)

--- onebusaway-siri-api/src/main/java/org/onebusaway/siri/model/DistanceExtensions.java
+++ onebusaway-siri-api/src/main/java/org/onebusaway/siri/model/DistanceExtensions.java
@@ -4,7 +4,5 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 @XStreamAlias("Extensions")
 public class DistanceExtensions {
-  public Double DistanceAlongRoute;
-  public Double DistanceFromCall;
-  public int StopsFromCall;
+  public Distances Distances;
 }
---
+++ onebusaway-siri-api/src/main/java/org/onebusaway/siri/model/Distances.java
@@ -0,0 +1,7 @@
+package org.onebusaway.siri.model;
+
+public class Distances {
+  public Double DistanceAlongRoute;
+  public Double DistanceFromCall;
+  public int StopsFromCall;
+}
